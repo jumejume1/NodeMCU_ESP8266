18d30d395e8db42827b0b6cda54b2b775e7676c3
jumejume1 <macbookair@MacbooKAirs-MacBook-Air.local>
2017-11-22 23:11:01 +0700

    NodeMCU ESP8266 How to read current time from Internet

---
+++ READ_TIME_FROM_INTERNET/READ_TIME_FROM_INTERNET.ino
@@ -0,0 +1,69 @@
+#include <ESP8266WiFi.h>
+#include <time.h>
+
+const char* ssid = "<your WiFi SSID>";
+const char* password = "<your WiFi Password>";
+
+int ledPin = 13;
+
+int timezone = 7 * 3600;
+int dst = 0;
+
+void setup() {
+  
+  pinMode(ledPin,OUTPUT);
+  digitalWrite(ledPin,LOW);
+
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Wifi connecting to ");
+  Serial.println( ssid );
+
+  WiFi.begin(ssid,password);
+
+  Serial.println();
+  Serial.print("Connecting");
+
+  while( WiFi.status() != WL_CONNECTED ){
+      delay(500);
+      Serial.print(".");        
+  }
+
+  digitalWrite( ledPin , HIGH);
+  Serial.println();
+
+  Serial.println("Wifi Connected Success!");
+  Serial.print("NodeMCU IP Address : ");
+  Serial.println(WiFi.localIP() );
+
+  configTime(timezone, dst, "pool.ntp.org","time.nist.gov");
+  Serial.println("\nWaiting for Internet time");
+
+  while(!time(nullptr)){
+     Serial.print("*");
+     delay(1000);
+  }
+  Serial.println("\nTime response....OK");   
+}
+
+void loop() {
+  
+  time_t now = time(nullptr);
+  struct tm* p_tm = localtime(&now);
+  Serial.print(p_tm->tm_mday);
+  Serial.print("/");
+  Serial.print(p_tm->tm_mon + 1);
+  Serial.print("/");
+  Serial.print(p_tm->tm_year + 1900);
+  
+  Serial.print(" ");
+  
+  Serial.print(p_tm->tm_hour);
+  Serial.print(":");
+  Serial.print(p_tm->tm_min);
+  Serial.print(":");
+  Serial.println(p_tm->tm_sec);
+  
+  delay(1000);
+
+}
